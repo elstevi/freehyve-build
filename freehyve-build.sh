55702a5535884d323033b40fc2bda18ccda7d487
Steven Douglas <steven@douglas-enterprises.com>
2018-05-06 21:38:32 -0700

    Change build numbers when using jenkins

--- freehyve-build.sh
+++ freehyve-build.sh
@@ -7,7 +7,12 @@ export WORKSPACE=$WORKSPACE
 export ARTIFACT_DIR="/root/build"
 export WORK_DIR="${WORKSPACE}/work"
 export VERSION="12.0-CURRENT"
+if [ -z ${BUILD_NUMBER+x} ]; then
 export FREEHYVE_VERS="0.0.1zi"
+else
+	FREEHYVE_VERSION="0.0.1-${BUILD_NUMBER}"
+fi
+
 export DIST_DROP_DIR="${ARTIFACT_DIR}/freebsd"
 export PACKAGE_DROP_DIR="${ARTIFACT_DIR}/packages"
 export IMAGE_DROP_DIR="${ARTIFACT_DIR}/image"

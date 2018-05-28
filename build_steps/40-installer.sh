d560e968d7b652ea48e6cc9ba4fceb46996159c4
Steven Douglas <steven@douglas-enterprises.com>
2018-05-28 15:37:11 -0700

    Build isos as well, all images need pv

--- build_steps/40-installer.sh
+++ build_steps/40-installer.sh
@@ -5,4 +5,7 @@ set -x
 cd ${WORKSPACE}/installer
 mkdir tmp work
 make -f Makefile.freehyve_installer
+make -f Makefile.freehyve_installer iso
+
 cp *.img ${INSTALLER_DROP_DIR}
+cp *.iso ${INSTALLER_DROP_DIR}
